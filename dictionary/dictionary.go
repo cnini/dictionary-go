647655340c853ba02a9b9f8c8d09ac1577e37dad
cnini <cynthia-zoely-nirina.rabemanantsoa@estiam.com>
2024-01-18 12:20:00 +0100

    Modifiez le package dictionary pour utiliser un fichier au lieu d'une map pour stocker les entrées du dictionnaire

--- dictionary/dictionary.go
+++ dictionary/dictionary.go
@@ -1,55 +1,63 @@
 package dictionary
 
-import (
-	"fmt"
-	"sort"
-)
-
-type Dictionary map[string]string
-
-func (d Dictionary) Add(word string, definition string) {
-	d[word] = definition
+type Dictionary struct {
+	Filename string
 }
 
-func (d Dictionary) Get(searchTerm string) (string, string) {
-	// If searchTerm is not a definition
-	if definition, found := d[searchTerm]; found {
-		return searchTerm, definition
-	}
-
-	// If searchTerm is a definition
-	for word, definition := range d {
-		if definition == searchTerm {
-			return word, definition
-		}
-	}
-
-	return "", ""
+type DictionaryEntry struct {
+	Word       string
+	Definition string
 }
 
-func (d Dictionary) Remove(termToRemove string) {
-	// Get the word and not the definition,
-	// even if termToRemove is definition
-	word, _ := d.Get(termToRemove)
-
-	delete(d, word)
+func NewDictionary(filename string) *Dictionary {
+	return &Dictionary{
+		Filename: filename,
+	}
 }
 
-func (d Dictionary) List() []string {
-	var sortedDictionary []string
+// func (d Dictionary) Add(word string, definition string) {
+// 	d[word] = definition
+// }
 
-	// Create a list version of the dictionnary to easily sort it
-	var listedDictionary []string
-	for word := range d {
-		listedDictionary = append(listedDictionary, word)
-	}
+// func (d Dictionary) Get(searchTerm string) (string, string) {
+// 	// If searchTerm is not a definition
+// 	if definition, found := d[searchTerm]; found {
+// 		return searchTerm, definition
+// 	}
 
-	sort.Strings(listedDictionary)
+// 	// If searchTerm is a definition
+// 	for word, definition := range d {
+// 		if definition == searchTerm {
+// 			return word, definition
+// 		}
+// 	}
 
-	for _, word := range listedDictionary {
-		definition := d[word]
-		sortedDictionary = append(sortedDictionary, fmt.Sprintf("%s: %s", word, definition))
-	}
+// 	return "", ""
+// }
 
-	return sortedDictionary
-}
+// func (d Dictionary) Remove(termToRemove string) {
+// 	// Get the word and not the definition,
+// 	// even if termToRemove is definition
+// 	word, _ := d.Get(termToRemove)
+
+// 	delete(d, word)
+// }
+
+// func (d Dictionary) List() []string {
+// 	var sortedDictionary []string
+
+// 	// Create a list version of the dictionnary to easily sort it
+// 	var listedDictionary []string
+// 	for word := range d {
+// 		listedDictionary = append(listedDictionary, word)
+// 	}
+
+// 	sort.Strings(listedDictionary)
+
+// 	for _, word := range listedDictionary {
+// 		definition := d[word]
+// 		sortedDictionary = append(sortedDictionary, fmt.Sprintf("%s: %s", word, definition))
+// 	}
+
+// 	return sortedDictionary
+// }
--- main.go
+++ main.go
@@ -1,43 +1,37 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/cnini/dictionary-go/dictionary"
-)
-
 func main() {
-	dictionary := make(dictionary.Dictionary)
+	// dictionary := make(dictionary.Dictionary)
 
-	dictionary.Add("Apprendre", "To learn")
-	dictionary.Add("Enseigner", "To teach")
-	dictionary.Add("Livre", "Book")
-	dictionary.Add("Crayon", "Pencil")
-	dictionary.Add("RAS", "Nothing to report")
-	dictionary.Add("MDR", "LOL")
-	dictionary.Add("Supprimer", "To remove")
+	// dictionary.Add("Apprendre", "To learn")
+	// dictionary.Add("Enseigner", "To teach")
+	// dictionary.Add("Livre", "Book")
+	// dictionary.Add("Crayon", "Pencil")
+	// dictionary.Add("RAS", "Nothing to report")
+	// dictionary.Add("MDR", "LOL")
+	// dictionary.Add("Supprimer", "To remove")
 
-	fmt.Println("-- French-English dictionary (after Add calls) ------")
-	sortedDictionary := dictionary.List()
-	for _, entry := range sortedDictionary {
-		fmt.Println(entry)
-	}
+	// fmt.Println("-- French-English dictionary (after Add calls) ------")
+	// sortedDictionary := dictionary.List()
+	// for _, entry := range sortedDictionary {
+	// 	fmt.Println(entry)
+	// }
 
-	searchTerm := "RAS"
-	word, definition := dictionary.Get(searchTerm)
-	if word != "" {
-		fmt.Printf("\n-- \"%s\"'s definition : %s. ------", word, definition)
-	} else {
-		fmt.Printf("\n-- \"%s\" does not exists. ------", searchTerm)
-	}
+	// searchTerm := "RAS"
+	// word, definition := dictionary.Get(searchTerm)
+	// if word != "" {
+	// 	fmt.Printf("\n-- \"%s\"'s definition : %s. ------", word, definition)
+	// } else {
+	// 	fmt.Printf("\n-- \"%s\" does not exists. ------", searchTerm)
+	// }
 
-	termToRemove := "Supprimer"
-	fmt.Printf("\n\n-- Removing \"%s\" and its definition. ------", termToRemove)
-	dictionary.Remove(termToRemove)
+	// termToRemove := "Supprimer"
+	// fmt.Printf("\n\n-- Removing \"%s\" and its definition. ------", termToRemove)
+	// dictionary.Remove(termToRemove)
 
-	fmt.Println("\n\n-- French-English dictionary (after Remove call) ------")
-	sortedDictionary = dictionary.List()
-	for _, entry := range sortedDictionary {
-		fmt.Println(entry)
-	}
+	// fmt.Println("\n\n-- French-English dictionary (after Remove call) ------")
+	// sortedDictionary = dictionary.List()
+	// for _, entry := range sortedDictionary {
+	// 	fmt.Println(entry)
+	// }
 }
